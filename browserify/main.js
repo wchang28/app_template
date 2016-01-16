52ad8b2284a07dcebd0a486069a1441aadbf8e8f
wchang28 <wchang28@hotmail.com>
2016-01-16 13:50:13 -0500

    add bloodhound

--- browserify/main.js
+++ browserify/main.js
@@ -2,5 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var MyMatch = require('./my_match');
 var TypeAhead = require('./typeahead');
+var $ = require('jquery');
+var BloodhoundFactory = require('bloodhound');
+var Bloodhound = BloodhoundFactory($);
 
 ReactDOM.render(<TypeAhead matchClass={MyMatch}/>, document.getElementById('test'));
