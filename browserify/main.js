7bff26e84f365f8415b71a3f64c6cad7117d9672
wchang28 <wchang28@hotmail.com>
2016-02-01 10:16:49 -0500

    added more options

--- browserify/main.js
+++ browserify/main.js
@@ -1,52 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var $ = require('jquery');
-var BloodhoundFactory = require('bloodhound');
-var Bloodhound = BloodhoundFactory($);
-require('eventsource-polyfill');
 
-/*
-var source = new EventSource("/api/event_stream");
-source.onmessage = function(event) {
-	var o = JSON.parse(event.data);
-	console.log('message: ' + JSON.stringify(o));
-};
-*/
+var GoogleSearchSuggestion = require('./googleSearchSuggestion');
 
-var MyMatch = require('./my_match');
+var AddressMatch = require('./addressMatch');
 var TypeAhead = require('./typeahead');
 
 function onQueryChanged(query, suggestionSelected) {
 	console.log('onQueryChanged(' + query + ',' + suggestionSelected + ')');
 }
 
-var datums =
-[
-	{"Id": "1", "lastName":"Chang", "firstName": "Wen"}
-	,{"Id": "2", "lastName":"Ku", "firstName": "Elva"}
-	,{"Id": "3", "lastName":"Chang", "firstName": "Winston"}
-	,{"Id": "4", "lastName":"Chang", "firstName": "Evelyn"}
-	,{"Id": "5", "lastName":"Chang", "firstName": "Juei"}
-	,{"Id": "6", "lastName":"Chang", "firstName": "Zoey"}
-	,{"Id": "7", "lastName":"Chang", "firstName": "Chung Hsia"}
-	,{"Id": "8", "lastName":"Liu", "firstName": "Ling Chu"}
-	,{"Id": "9", "lastName":"Chen", "firstName": "Tiwei"}
-	,{"Id": "10", "lastName":"Sun", "firstName": "James"}
-]
-			
-var engine = new Bloodhound({
-	initialize: false,
-	local: datums,
-	identify: (obj) => obj.Id,
-	queryTokenizer: Bloodhound.tokenizers.whitespace,
-	datumTokenizer: (datum) => [datum.Id, datum.lastName, datum.firstName]
-});
+var engine = new GoogleSearchSuggestion();
 
-var promise = engine.initialize();
-
-promise.done(function() {
-	console.log('ready to go!');
-	ReactDOM.render(<TypeAhead dropDownContentClass={MyMatch} identity={(o) => o.Id} suggestionEngine={engine} minCharToSearch={2} onQueryChanged={onQueryChanged} placeholder="Type something" roundSize="medium"/>, document.getElementById('test'));
-}).fail(function() {
-	console.log('err, something went wrong :('); 
-});
+ReactDOM.render(<TypeAhead dropDownContentClass={AddressMatch} identity={(o) => o} suggestionEngine={engine} minCharToSearch={1} onQueryChanged={onQueryChanged} placeholder="Type anything" roundSize="medium"/>, document.getElementById('test'));
