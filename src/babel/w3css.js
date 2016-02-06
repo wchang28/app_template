161571be16dd19ca0f3418ec35c649136e7c2105
wchang28 <wchang28@hotmail.com>
2016-02-06 08:41:34 -0500

    added more style

--- src/babel/w3css.js
+++ src/babel/w3css.js
@@ -2,11 +2,13 @@
 	module.exports = factory();
 })(this, function() {
 	var $ = require('jquery');
+	function addStyle(srcStyle, newStyle) {return $.extend(true, {}, srcStyle, newStyle);}
 	var ret = {};
-	ret.tableStyle = {borderCollapse: 'collapse', borderSpacing: '0'};
-	var tdStyle = {border:'none', paddingLeft: '8px', paddingTop: '6px', paddingBottom: '6px', paddingRight: '8px', display:'table-cell',textAlign:'left', verticalAlign:'top'};
-	var firstTdStyle = $.extend(true, {}, tdStyle);
-	firstTdStyle.paddingLeft = '16px';
+	var baseStyle = {boxSizing:'border-box'};
+	ret.tableStyle = addStyle(baseStyle, {display:'table',borderCollapse:'collapse',borderSpacing:'0',width:'100%'});
+	var tdStyle = addStyle(baseStyle, {display:'table-cell',border:'none',paddingLeft:'8px',paddingTop:'6px',paddingBottom:'6px',paddingRight:'8px',textAlign:'left', verticalAlign:'top'});
+	var firstTdStyle = addStyle(tdStyle, {paddingLeft:'16px'});
 	ret.getTdStyle = function(columnIndex) {return (columnIndex == 0 ? firstTdStyle : tdStyle);};
+	
 	return ret;
 });
