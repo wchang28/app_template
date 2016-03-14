adc6dfb69893576303db8a1eedc6a0f00af63e34
wchang28 <wchang28@hotmail.com>
2016-03-13 22:09:02 -0400

    add location support

--- src/babel/googleSearchSuggestion.js
+++ src/babel/googleSearchSuggestion.js
@@ -1,12 +1,26 @@
 var $ = require('jquery');
 
 function GoogleSearchSuggestion() {
+	function position2JSON(position) {
+		var pos = {
+			coords: {}
+			timestamp: position.timestamp
+		};
+		pos.coords.latitude = position.coords.latitude;
+		pos.coords.longitude = position.coords.longitude;
+		pos.coords.accuracy = position.coords.accuracy;
+		if (typeof position.coords.altitude === 'number') pos.coords.altitude = position.coords.altitude;
+		if (typeof position.coords.altitudeAccuracy === 'number') pos.coords.altitudeAccuracy = position.coords.altitudeAccuracy;
+		if (typeof position.coords.heading === 'number') pos.coords.heading = position.coords.heading;
+		if (typeof position.coords.speed === 'number') pos.coords.speed = position.coords.speed;
+		return pos;
+	}
 	function getGeolocation(onDone) {
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(function(position) {
-				console.log('latitude='+position.coords.latitude);
-				console.log('longitude='+position.coords.longitude);
-				if (typeof onDone === 'function') onDone(position);
+				var pos = position2JSON(position);
+				console.log('position='+JSON.stringify(pos));
+				if (typeof onDone === 'function') onDone(pos);
 			});
 		} else {
 			if (typeof onDone === 'function') onDone(null);
