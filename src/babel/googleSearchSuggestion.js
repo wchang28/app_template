53afb06fae8aa6357fca1d91949e2407c37d8974
wchang28 <wchang28@hotmail.com>
2016-03-13 21:46:46 -0400

    add location support

--- src/babel/googleSearchSuggestion.js
+++ src/babel/googleSearchSuggestion.js
@@ -1,32 +1,36 @@
 var $ = require('jquery');
 
 function GoogleSearchSuggestion() {
-	var me = this;
-	this.position = null;
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function(position) {
-			console.log('position='+JSON.stringify(position));
-			me.position = position;
-		});
+	function getGeolocation(onDone) {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function(position) {
+				console.log('position='+JSON.stringify(position));
+				if (typeof onDone === 'function') onDone(position);
+			});
+		} else {
+			if (typeof onDone === 'function') onDone(null);
+		}		
 	}
 	this.search = function(query, onSuggestion) {
 		if (query === '') {
 			if (typeof onSuggestion === 'function') onSuggestion([]);
 		} else {
-			var data = {
-				q: query
-				,position: me.position
-			};
-			$.ajax({
-				type: "POST",
-				url: '/api/get_suggestion/',
-				data: JSON.stringify(data),
-				contentType: "application/json; charset=utf-8",
-				dataType: "json"
-			}).done(function(suggestions) {
-				if (typeof onSuggestion === 'function') onSuggestion(suggestions);
-			}).fail(function(err){
-				if (typeof onSuggestion === 'function') onSuggestion([]);
+			getGeolocation(function(position) {
+				var data = {
+					q: query
+					,position: position
+				};
+				$.ajax({
+					type: "POST",
+					url: '/api/get_suggestion/',
+					data: JSON.stringify(data),
+					contentType: "application/json; charset=utf-8",
+					dataType: "json"
+				}).done(function(suggestions) {
+					if (typeof onSuggestion === 'function') onSuggestion(suggestions);
+				}).fail(function(err){
+					if (typeof onSuggestion === 'function') onSuggestion([]);
+				});				
 			});
 		}
 	};
